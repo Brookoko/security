e4ab8886af1d5e91f46d3aafc4463ff3c069303c
bohdan.fedorchenko <bogdan.fedorchenko@tatemgames.com>
2021-11-14 22:58:52 +0200

    Moved file string encoding to separate class

--- security/Program.cs
+++ security/Program.cs
@@ -1,39 +1,39 @@
 ﻿namespace security
 {
-    using System;
-    using System.Collections.Generic;
     using System.IO;
     using System.Linq;
-    using System.Text;
 
     class Program
     {
+        private static readonly StringEncoder StringEncoder = new();
+
         static void Main(string[] args)
         {
+            FromBinaryToText();
             DecodeSingleByte();
         }
 
         private static void DecodeSingleByte()
         {
             var text = File.ReadAllText(GetPathInProject("lab1single.txt"));
-            var bytes = ConvertHexToBytes(text);
+            var bytes = StringEncoder.GetBytes(text);
             var results = new string[256];
             for (var i = 0; i < 256; i++)
             {
-                var result = new byte[bytes.Length];
-                for (var j = 0; j < bytes.Length; j++)
-                {
-                    result[j] = (byte)(bytes[j] ^ i);
-                }
-                var decipherText = Encoding.ASCII.GetString(result);
-                results[i] = decipherText;
+                var result = Xor(bytes, (byte)i);
+                results[i] = StringEncoder.GetString(result);
             }
             var aggregate = "";
             for (var i = 0; i < results.Length; i++)
             {
                 aggregate += $"{i}\n{results[i]}\n<---------->\n";
             }
-            File.WriteAllText(GetPathInProject($"lab1single-decoded.txt"), aggregate);
+            File.WriteAllText(GetPathInProject("lab1single-decoded.txt"), aggregate);
+        }
+
+        private static byte[] Xor(byte[] bytes, byte k)
+        {
+            return bytes.Select(b => (byte)(b ^ k)).ToArray();
         }
 
         private static string GetPathInProject(string path)
@@ -41,35 +41,13 @@
             return $"../../../{path}";
         }
 
-        private static byte[] ConvertHexToBytes(string text)
-        {
-            return Enumerable.Range(0, text.Length)
-                .Where(x => x % 2 == 0)
-                .Select(x => Convert.ToByte(text.Substring(x, 2), 16))
-                .ToArray();
-        }
-
         private static void FromBinaryToText()
         {
-            var data = File.ReadAllText(GetPathInProject("lab1.txt"));
-            var bytes = GetBytesFromBinaryString(data);
-            var text = Encoding.ASCII.GetString(bytes);
-            File.WriteAllText(GetPathInProject("lab1decoded.txt"), text);
-        }
-
-        public static byte[] GetBytesFromBinaryString(string binary)
-        {
-            var bytes = new List<byte>();
-            for (var i = 0; i < binary.Length; i += 8)
-            {
-                if (i + 8 >= binary.Length)
-                {
-                    continue;
-                }
-                var substring = binary.Substring(i, 8);
-                bytes.Add(Convert.ToByte(substring, 2));
-            }
-            return bytes.ToArray();
+            var text = File.ReadAllText(GetPathInProject("lab1.txt"));
+            var bytes = StringEncoder.GetBytes(text);
+            var decoded = StringEncoder.GetString(bytes);
+            var baseString = StringEncoder.GetBytes(decoded);
+            File.WriteAllBytes(GetPathInProject("lab1base-decoded.txt"), baseString);
         }
     }
 }
---
+++ security/StringEncoder.cs
@@ -0,0 +1,63 @@
+namespace security
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class StringEncoder
+    {
+        public string GetString(byte[] bytes)
+        {
+            return Encoding.UTF8.GetString(bytes);
+        }
+
+        public byte[] GetBytes(string text)
+        {
+            if (IsBinary(text))
+            {
+                return FromBinary(text);
+            }
+            if (IsHex(text))
+            {
+                return FromHex(text);
+            }
+            var buffer = new Span<byte>(new byte[text.Length]);
+            if (Convert.TryFromBase64String(text, buffer, out var parsed))
+            {
+                return buffer.ToArray().Take(parsed).ToArray();
+            }
+            return FromSimpleText(text);
+        }
+
+        private bool IsBinary(string text)
+        {
+            return text.All(c => c == 48 || c == 49);
+        }
+
+        private byte[] FromBinary(string text)
+        {
+            return Enumerable.Range(0, text.Length)
+                .Where(x => x % 8 == 0)
+                .Select(x => Convert.ToByte(text.Substring(x, 8), 2))
+                .ToArray();
+        }
+
+        private bool IsHex(string text)
+        {
+            return text.All(c => c >= 48 && c <= 57 || c >= 97 && c <= 102);
+        }
+
+        private byte[] FromHex(string text)
+        {
+            return Enumerable.Range(0, text.Length)
+                .Where(x => x % 2 == 0)
+                .Select(x => Convert.ToByte(text.Substring(x, 2), 16))
+                .ToArray();
+        }
+
+        private byte[] FromSimpleText(string text)
+        {
+            return Encoding.UTF8.GetBytes(text);
+        }
+    }
+}
