e548eade85bae2b3454eff90f99e0003d9416e3b
bohdan.fedorchenko <bogdan.fedorchenko@tatemgames.com>
2021-11-28 17:17:53 +0200

    Updated parameters for genetic algorithm

--- Lab1/PolySubstitution/PolySubstitutionDecipher.cs
+++ Lab1/PolySubstitution/PolySubstitutionDecipher.cs
@@ -4,10 +4,10 @@ namespace security.PolySubstitution
 
     public class PolySubstitutionDecipher : GeneticAlgorithm<PolySubstitutionKey>
     {
-        protected override int NumberOfIterations => 8000;
-        protected override int PopulationSize => 300;
-        protected override int BestSize => 60;
-        protected override double MutationProbability => 0.4;
+        protected override int NumberOfIterations => 10000;
+        protected override int PopulationSize => 500;
+        protected override int BestSize => 100;
+        protected override double MutationProbability => 0.3;
         protected override int NumberOfMutations => 2;
 
         private readonly int keyLength;
